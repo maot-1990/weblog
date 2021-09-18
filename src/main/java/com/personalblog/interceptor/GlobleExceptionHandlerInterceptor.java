1d09d49afe63cbcc71301a21b4600166bc2ab498
779396604@qq.com <162Aling>
2021-09-18 18:20:27 +0800

    给力的优化

--- src/main/java/com/personalblog/interceptor/GlobleExceptionHandlerInterceptor.java
+++ src/main/java/com/personalblog/interceptor/GlobleExceptionHandlerInterceptor.java
@@ -9,20 +9,22 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @ControllerAdvice
 public class GlobleExceptionHandlerInterceptor {
 
     @ExceptionHandler({Exception.class, RuntimeException.class})
-    public Object exceptionHandler(HttpServletRequest req, Exception e) {
+    public Object exceptionHandler(HttpServletRequest req, HttpServletResponse res, Exception e) {
         if (e instanceof BaseException) {
             req.getSession().setAttribute("msgError", e.getMessage());
-            /*return new ResponseEntity<>(BaseResult.fail(be.getErrorCode(), be.getMessage()),
-                    HttpStatus.OK);*/
+            res.setStatus(HttpServletResponse.SC_NOT_FOUND);
         } else {
             req.getSession().setAttribute("msgError", "系统异常，请稍后重试");
+            res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
+
         log.error(e.getMessage());
         return "404-error";
     }
